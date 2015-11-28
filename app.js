12f6b4fc4eab873f5cb88bafae03f1f818f91be1
etmoore <etmoore@gmail.com>
2015-11-28 16:23:51 -0700

    display city, temp, and description

--- app.js
+++ app.js
@@ -5,28 +5,29 @@ var httpRequest;
 function makeWeatherRequest(lat, lon){
   httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = displayWeather;
-  httpRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=2de143494c0b295cca9337e1e96b00e0');
+  httpRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=2de143494c0b295cca9337e1e96b00e0&units=imperial');
   httpRequest.send();
 }
 
 function displayWeather(){
   if (httpRequest.readyState === XMLHttpRequest.DONE){
     if (httpRequest.status === 200){
-      console.log(httpRequest.responseText);
+      var json= JSON.parse(httpRequest.responseText);
+      var html = '';
+      html += "City: " + json.name + "<br>";
+      html += "Temperature: " + json.main.temp + " degrees <br>";
+      html += "Description: " + json.weather[0].description + "<br>";
+      document.getElementsByClassName('weatherbox')[0].innerHTML = html;
     }
   }
 }
 
-function getGeoLocation(){
-  navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position.coords.latitude, position.coords.longitude);
+function init(){
+  navigator.geolocation.getCurrentPosition(function(position){
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    makeWeatherRequest(lat, lon);
   });
-  // navigator.geoLocation.getCurrentPosition(function(position){
-  //   lat = position.coords.latitude;
-  //   lon = position.coords.longitude;
-  //   console.log(lat, lon);
-  // });
 };
 
-getGeoLocation();
-makeWeatherRequest(35, 139);
+init();
