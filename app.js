791cd6ca11adb9e1383f80f1926210ce2fb54b26
etmoore <etmoore@gmail.com>
2015-11-28 18:06:52 -0700

    set white background and google fonts

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ var lat;
 var lon;
 var httpRequest;
 
-function makeWeatherRequest(lat, lon){
+function makeRequest(lat, lon){
   httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = displayWeather;
-  httpRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=2de143494c0b295cca9337e1e96b00e0&units=imperial');
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=2de143494c0b295cca9337e1e96b00e0&units=imperial'
+  httpRequest.open('GET', url);
   httpRequest.send();
 }
 
 function displayWeather(){
   if (httpRequest.readyState === XMLHttpRequest.DONE){
     if (httpRequest.status === 200){
-      var json= JSON.parse(httpRequest.responseText);
+      var json = JSON.parse(httpRequest.responseText);
       var html = '';
-      html += "City: " + json.name + "<br>";
-      html += "Temperature: " + json.main.temp + " degrees <br>";
-      html += "Description: " + json.weather[0].description + "<br>";
+      html += "<p class='city'>" + json.name + "</p>";
+      html += "<p class='temperature'>" + json.main.temp + " degrees </p>";
+      html += "<p class='description'>" + json.weather[0].description + "</p>";
       document.getElementsByClassName('weatherbox')[0].innerHTML = html;
     }
   }
@@ -26,7 +27,7 @@ function init(){
   navigator.geolocation.getCurrentPosition(function(position){
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    makeWeatherRequest(lat, lon);
+    makeRequest(lat, lon);
   });
 };
 
